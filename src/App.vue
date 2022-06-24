8a21fde4540c190ca991dcac2d5603605ecd0d51
xiongguangsen <3096429133@qq.com>
2022-06-24 10:15:16 +0800

    新增了button组件和按需显示组件，实现了按需显示小功能，和切换两个功能

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
 <template>
   <div id="app">
-        <addDelDemo></addDelDemo>
+        <div class="app-button">
+            <buttonChange @changeComponent="changeComponent"></buttonChange>
+        </div>
+        <keep-alive>
+            <component :is="pageChange"></component>
+        </keep-alive>
+        
   </div>
 </template>
 
 <script>
+import showNeed from '@/components/showNeed.vue'
 import addDelDemo from '@/components/addDelDemo.vue'
+import buttonChange from '@/components/buttonChange.vue'
 import "@/assets/less/global.less"
 export default {
   name: 'App',
   components: {
-    addDelDemo
+    addDelDemo,
+    showNeed,
+    buttonChange
+  },
+  data() {
+    return {
+        pageChange: addDelDemo
+    }
+  },
+  methods: {
+    changeComponent(value) {
+        this.pageChange = value
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.app-button {
+    text-align: center;
+    position: fixed;
+    left: 50%;
+    top: 10%;
+    transform: translate(-50%);
+}
 
 </style>
